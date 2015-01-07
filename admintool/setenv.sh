442cc0ad507ad60c8838b61b619aa5272a5c61a7
Bjorn Boe <bboe@pivotal.io>
2015-01-07 11:03:05 -0500

    Small update

--- admintool/setenv.sh
+++ admintool/setenv.sh
@@ -1,2 +1,2 @@
 #export SERVER_URL=http://admin:admin@localhost:8080
-export SERVER_URL=http://admin:admin@service-broker.10.244.0.34.xip.io
+export SERVER_URL=http://admin:admin@service-repository.10.244.0.34.xip.io
